e10963551a15630e395db7ccf0ac6c4c4551eb7e
Basina Pinakapaani <142944580+Paani143@users.noreply.github.com>
2023-11-08 22:33:43 -0600

    updated

--- controllers/furniture.js
+++ controllers/furniture.js
@@ -1,3 +1,4 @@
+const furniture = require('../models/furniture');
 var Furniture = require('../models/furniture');
 // List of all furnitures
 exports.furniture_list = async function(req, res) {
@@ -27,3 +28,39 @@ res.send('NOT IMPLEMENTED: Furniture delete DELETE ' + req.params.id);
 exports.furniture_update_put = function(req, res) {
 res.send('NOT IMPLEMENTED: Furniture update PUT' + req.params.id);
 };
+
+exports.furniture_create_post = async function(req, res) {
+    console.log(req.body)
+    let document = new furniture();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"costume_type":"goat", "cost":12, "size":"large"}
+    document.furniture_name = req.body.furniture_name;
+    document.furniture_specification = req.body.furniture_specification;
+    document.furniture_cost = req.body.furniture_cost;
+    try{
+    let result = await document.save();
+    res.send(result);
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+    };
+    
+
+// VIEWS
+// Handle a show all view
+exports.furniture_view_all_Page = async function(req, res) {
+    try{
+    theFurnitures = await furniture.find();
+    res.render('furniture', { title: 'Furniture Search Results', results: theFurnitures });
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+    };
+    
+    
